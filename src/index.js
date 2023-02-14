fb846bfe83c637d90a6d002c0730966364d7bda3
romanusovich1 <romanusovich1@gmail.com>
2023-02-14 10:17:28 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2 !== 0) return false;
+  let flag = true;
+  while (flag) {
+    flag = false;
+    for (let bracket in bracketsConfig) {
+      for (let i = 0; i <= str.length; i++) {
+        if (bracketsConfig[bracket][0] === str[i] && bracketsConfig[bracket][1] === str[i + 1]) {
+          let brackets = bracketsConfig[bracket][0] + bracketsConfig[bracket][1];
+          str = str.replace(brackets, '');
+          
+          i = 0;
+          flag = true;
+        }
+      }
+    }
+  }
+  if (str.length === 0) return true;
+  else return false;
 }
